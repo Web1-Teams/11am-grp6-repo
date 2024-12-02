42c54a8956df311964d2eed30addad3e96738233
Hamza Zarour <s12323435@stu.najah.edu>
2024-12-02 18:32:22 +0200

    intial react setup
    pull this into your device then in git bash write npm install because of some files that will not be in your device

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Pages/Home";
-import Services from "./components/Pages/Services";
-import Products from "./components/Pages/Products";
-import SignUp from "./components/Pages/SignUp";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact Component={Home} />
-          <Route path="/services" Component={Services}/>
-          <Route path="/Products" Component={Products}/>
-          <Route path="/Sign-up" Component={SignUp}/>
-        </Routes>
-      </Router>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
