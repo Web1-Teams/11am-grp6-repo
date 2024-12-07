d4a5c671cda7b2c55f2f501c61955d799ad8e749
dareen-2006 <s12323368@stu.najah.edu>
2024-12-07 13:38:06 +0200

    dareen-task-done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+
+import './App.css';
+import Section from './components/Section';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 function App() {
-  return <div></div>;
+return (   <>
+<Section />
+</>)
 }
-
 export default App;
