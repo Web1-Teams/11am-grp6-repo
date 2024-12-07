ed3abae91be88522127f9cb66cff095e63b088ac
s12325463@stu.najah.edu <s12325463@stu.najah.edu>
2024-12-07 06:45:10 +0200

    suhad Navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div></div>;
+  return <div>
+   <NavBar BrandName="eATMe" i1="Home" i2="Something Else2" i3="Something Else3" />
+
+  </div>;
 }
 
 export default App;
