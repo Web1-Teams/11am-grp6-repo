c378df2db654d173389ac2337fe46ba967c67ab6
s12325463@stu.najah.edu <s12325463@stu.najah.edu>
2024-12-13 22:09:19 +0200

    Search Bar added

--- src/components/Slideer.js
+++ src/components/Slideer.js
@@ -4,6 +4,7 @@ import img10 from "./img/img-home.jpg";
 import img11 from "./img/img-1.jpg";
 import img12 from "./img/img-4.jpg";
 import "./Slider.css";
+import SearchBar from "./SearchBar";
 
 const Slider = () => {
   return (
@@ -93,6 +94,10 @@ const Slider = () => {
           <img src={img12} className="grp6-img d-block w-100" alt="Slide 5" />
         </div>
       </div>
+         {/* New updates  */}
+         <div className="Slider-Searchbar">
+          <SearchBar/>
+        </div>
     </div>
   );
 };
