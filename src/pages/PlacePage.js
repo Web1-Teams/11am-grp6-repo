affe875758173a7c3b82604e764b8cee488a5c72
remaa123 <remaayosef68@gmail.com>
2024-12-21 21:54:31 +0200

    modifying the css js for placepage footer and footer-cat

--- src/pages/PlacePage.js
+++ src/pages/PlacePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import '../pages/PlacePage.css';
+import "./PlacePage.css";
 import PlacePageSlider from '../components/PlacePageSlider';
 import Footer from '../components/Footer';
 import Footer_cat from '../components/Footer_cat';
